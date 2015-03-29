80c8809a76b50e44488347c922c7f368b0ee07dc
tiagob <tiago.bndr@gmail.com>
2015-03-29 11:12:07 -0700

    Base todo flask react app.

---
+++ app.py
@@ -0,0 +1,26 @@
+import flask
+import flask.ext.sqlalchemy
+import flask.ext.restless
+
+# Create the Flask application and the Flask-SQLAlchemy object.
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode)
+
+# Create the database tables.
+db.create_all()
+
+# Create the Flask-Restless API manager.
+manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+
+# Create API endpoints, which will be available at /api/<tablename> by
+# default. Allowed HTTP methods can be specified as well.
+manager.create_api(Todo, methods=['GET', 'POST', 'DELETE'])
+
+# start the flask loop
+app.run()
